e3ca8bcf5bcb07cf6346983f9b01b50aec655222
marcialfps <marcialfco@live.com>
2020-01-02 18:32:06 +0000

    Added session control in counter action.

--- src/main/java/com/miw/presentation/actions/CounterAction.java
+++ src/main/java/com/miw/presentation/actions/CounterAction.java
@@ -3,33 +3,47 @@ package com.miw.presentation.actions;
 import java.util.Map;
 
 import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 import org.apache.struts2.interceptor.ApplicationAware;
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.miw.model.Counter;
 import com.opensymphony.xwork2.ActionSupport;
 
-@Result(name="success", location="login-success.jsp" )
+@Results({
+	@Result(name="success", location="login-success.jsp" ),
+	@Result(name = "error", location = "/index.jsp")
+})
 
-public class CounterAction extends ActionSupport implements ApplicationAware {
+
+public class CounterAction extends ActionSupport implements ApplicationAware, SessionAware {
 
 	private static final long serialVersionUID = 3754807639524838051L;
 	private Map<String, Object> application = null;
+	private Map<String, Object> session = null;
 	
 	@Override
 	public String execute() throws Exception {
-		Counter counter = (Counter) application.get("counter");
-		if ( counter == null )
-		{
-			counter = new Counter();
+		if (session.containsKey("loginInfo")) {
+			Counter counter = (Counter) application.get("counter");
+			if ( counter == null )
+			{
+				counter = new Counter();
+			}
+			counter.increment();
+			application.put("counter", counter);
+			return SUCCESS; 
+		} else {
+			return ERROR;
 		}
-		counter.increment();
-		application.put("counter", counter);
-		return SUCCESS;
 	}
 
 	public void setApplication(Map<String, Object> application) {
 		this.application = application;
 	}
 
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
+	}
 	
 }
